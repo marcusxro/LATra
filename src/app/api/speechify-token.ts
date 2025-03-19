78faa84558dc9e02c48228de25f41dec63cc1876
Marcus Salopaso <114456732+marcusxro@users.noreply.github.com>
2025-03-19 23:26:03 +0800

    Update speechify-token.ts

--- src/app/api/speechify-token.ts
+++ src/app/api/speechify-token.ts
@@ -1,13 +1,11 @@
-// pages/api/speechify-token.ts
+
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method !== "POST") {
         return res.status(405).json({ error: "Method Not Allowed" });
     }
-
-    // Your Speechify API logic goes here
-    const accessToken = "9XQ0fTnV8w2V4RKyoq1bKCyUh3jxBse9pCb6v-cdP7Q=";
+    const accessToken ="token";
 
     if (!accessToken) {
         return res.status(500).json({ error: "Missing Speechify access token" });
